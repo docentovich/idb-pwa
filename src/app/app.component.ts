fb1c5ff7e2fedebeea4cdde80774f1786e11a026
andrey prozorov <andrey.prozorov@segmento.ru>
2024-01-11 18:37:25 +0300

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { fetchCached } from './app.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular16';
+  data = {};
+
+  constructor(private http: HttpClient) {
+  }
+
+  public ngOnInit(): void {
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+    // this.http.get(`https://jsonplaceholder.typicode.com/todos/1`)
+    //     .subscribe(data => {
+    //       this.data = data
+    //     })
+
+    fetchCached('//localhost:8080/api/todo-list', { method: 'POST' })
+        .then((data) => this.data = data)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,57 @@
+import { HttpClientModule, HttpResponse } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AsyncDatabase, IndexedDB } from '@ezzabuzaid/document-storage';
+import { AsyncCollection } from '@ezzabuzaid/document-storage/async/async.collection';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+debugger;
+export class HttpCacheEntry {
+  constructor(
+      public id: number,
+      public url: string,
+      public value: any
+  ) { }
+}
+
+const db = new AsyncDatabase(new IndexedDB('cache'))
+const collection = db.collection('CACHE') as unknown as AsyncCollection<HttpCacheEntry>;
+
+export const fetchCached = async (resource: RequestInfo, config = { method: 'GET' }) => {
+  debugger;
+  console.log(resource, config);
+  const url = typeof resource === 'string' ? resource : resource.url;
+  const requestString = (config.method ?? 'GET') + url
+
+  const cached = await collection.get((entry) => entry.url === requestString);
+
+  if(cached) {
+    return cached.value;
+  }
+
+  const response = await (await fetch(resource, config)).json();
+  const entry = new HttpCacheEntry(1, requestString, response);
+  await collection.set(entry);
+
+  return response;
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
